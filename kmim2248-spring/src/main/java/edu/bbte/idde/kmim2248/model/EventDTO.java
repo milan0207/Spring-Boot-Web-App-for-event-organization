4a7b68b829681dc0a55fa2decdbba355e7a12b5b
Koncsard Milan <milan2003koncsard@gmail.com>
2024-12-13 11:24:07 +0200

    Lab4 minor improvements on DTO model

--- kmim2248-spring/src/main/java/edu/bbte/idde/kmim2248/model/EventDTO.java
+++ kmim2248-spring/src/main/java/edu/bbte/idde/kmim2248/model/EventDTO.java
@@ -5,9 +5,12 @@ import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @AllArgsConstructor
@@ -20,14 +23,17 @@ public class EventDTO {
     private String name;
 
     @NotBlank(message = "Place is mandatory")
+    @Size(min = 3, max = 50, message = "Place must be between 3 and 50 characters")
     private String place;
 
     @NotNull(message = "Date is mandatory")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
     private Boolean online;
 
     @NotNull(message = "Duration is mandatory")
+    @Positive(message = "Duration must be positive")
     private int duration;
 
     public Boolean isOnline() {
